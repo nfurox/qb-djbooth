498c8374955e3a3dd999e11b41b75576699d501c
nfurox <38051034+nfurox@users.noreply.github.com>
2021-10-31 13:18:37 +0000

    Update config.lua
    Wokring

--- config.lua
+++ config.lua
@@ -4,13 +4,13 @@ Config.DefaultVolume = 0.1 -- Accepted values are 0.01 - 1
 
 Config.Locations = {
     ['vanilla'] = {
-        ['job'] = 'unicorn', -- Required job to use booth
-        ['radius'] = 50, -- The radius of the sound from the booth
-        ['coords'] = vector3() -- Where the booth is located
+        ['job'] = 'vanilla', -- Required job to use booth
+        ['radius'] = 30, -- The radius of the sound from the booth
+        ['coords'] = vector3(131.36, -1286.94, 29.26) -- Where the booth is located
     },
-    ['other'] = {
-        ['job'] = 'other',
-        ['radius'] = 50,
-        ['coords'] = vector3()
+    ['thelost'] = {
+        ['job'] = 'thelost',
+        ['radius'] = 20,
+        ['coords'] = vector3(983.79, -132.3, 78.89)
     }
 }
